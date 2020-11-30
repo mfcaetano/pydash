d9388dbfe0e2f7c310e47fcc6262d3f7ea474b55
Marcos F. Caetano <mfcaetano@unb.br>
2020-11-30 12:19:09 -0300

    making Whiteboard attributes private. Access only through gets and sets methods.

--- base/whiteboard.py
+++ base/whiteboard.py
@@ -23,40 +23,40 @@ class Whiteboard:
             raise Exception('This class is a singleton!')
         else:
             Whiteboard.__instance = self
-            self.buffer = []
-            self.playback = []
-            self.playback_qi = []
-            self.playback_pauses = []
-            self.playback_buffer_size = []
-            self.playback_segment_size_time_at_buffer = []
+            self.__buffer = []
+            self.__playback = []
+            self.__playback_qi = []
+            self.__playback_pauses = []
+            self.__playback_buffer_size = []
+            self.__playback_segment_size_time_at_buffer = []
             # partial segment size time at buffer list
-            self.partial_sstb = []
-            self.max_buffer_size = 0
-            self.amount_video_to_play = 0
+            self.__partial_sstb = []
+            self.__max_buffer_size = 0
+            self.__amount_video_to_play = 0
 
     def add_buffer(self, buffer):
-        self.buffer = buffer
+        self.__buffer = buffer
 
     def add_amount_video_to_play(self, amount_video_to_play):
-        self.amount_video_to_play = amount_video_to_play
+        self.__amount_video_to_play = amount_video_to_play
 
     def add_max_buffer_size(self, max_buffer_size):
-        self.max_buffer_size = max_buffer_size
+        self.__max_buffer_size = max_buffer_size
 
     def add_playback_qi(self, playback_qi):
-        self.playback_qi = playback_qi
+        self.__playback_qi = playback_qi
 
     def add_playback_pauses(self, pauses):
-        self.playback_pauses = pauses
+        self.__playback_pauses = pauses
 
     def add_playback_buffer_size(self, buffer_size):
-        self.playback_buffer_size = buffer_size
+        self.__playback_buffer_size = buffer_size
 
     def add_playback_history(self, playback):
-        self.playback = playback
+        self.__playback = playback
 
     def add_playback_segment_size_time_at_buffer(self, segment_size_time_at_buffer):
-        self.playback_segment_size_time_at_buffer = segment_size_time_at_buffer
+        self.__playback_segment_size_time_at_buffer = segment_size_time_at_buffer
 
     def get_playback_segment_size_time_at_buffer(self):
         """
@@ -69,36 +69,36 @@ class Whiteboard:
         pos = 0
 
         try:
-            pos = [x[1] for x in self.playback_segment_size_time_at_buffer].index(-1, max(len(self.partial_sstb)-1, 0))
+            pos = [x[1] for x in self.__playback_segment_size_time_at_buffer].index(-1, max(len(self.__partial_sstb) - 1, 0))
         except:
-            pos = len(self.playback_segment_size_time_at_buffer)
+            pos = len(self.__playback_segment_size_time_at_buffer)
 
-        plist = [round(x[1] - x[0], 6) for x in self.playback_segment_size_time_at_buffer[len(self.partial_sstb):pos]]
-        self.partial_sstb = self.partial_sstb + plist
+        plist = [round(x[1] - x[0], 6) for x in self.__playback_segment_size_time_at_buffer[len(self.__partial_sstb):pos]]
+        self.__partial_sstb = self.__partial_sstb + plist
 
-        return tuple(self.partial_sstb)
+        return tuple(self.__partial_sstb)
 
     def get_buffer(self):
-        return tuple(self.buffer)
+        return tuple(self.__buffer)
 
     def get_amount_video_to_play(self):
         """
-        It returns the total amount of video stored in the buffer that still will be played
+        It returns the total amount of video stored in the __buffer that still will be played
         """
-        return self.amount_video_to_play
+        return self.__amount_video_to_play
 
     def get_max_buffer_size(self):
         """
-        Returns the maximum buffer size. The download will stop after this amount will be achieved
+        Returns the maximum __buffer size. The download will stop after this amount will be achieved
         """
-        return self.max_buffer_size
+        return self.__max_buffer_size
 
     def get_playback_qi(self):
         """
         It returns a tuples list of time and QI's segments already played by the Player.
         The time represents the moment when a QI segment was consumed (played) by the Player.
         """
-        return tuple(self.playback_qi)
+        return tuple(self.__playback_qi)
 
     def get_playback_pauses(self):
         """
@@ -107,21 +107,21 @@ class Whiteboard:
         the pauses represents the lenght of this pauses.
         """
 
-        return tuple(self.playback_pauses)
+        return tuple(self.__playback_pauses)
 
     def get_playback_buffer_size(self):
         """
-        It returns a tuples list of time and buffer size during the playing video.
-        The time represents the moment when the buffer size was measured.
+        It returns a tuples list of time and __buffer size during the playing video.
+        The time represents the moment when the __buffer size was measured.
         """
 
-        return tuple(self.playback_buffer_size)
+        return tuple(self.__playback_buffer_size)
 
     def get_playback_history(self):
         """
-        It returns a tuples list of time and playback history happened during
+        It returns a tuples list of time and __playback history happened during
         the playing video. The time represents the moment when was measured the possible
-        to play or not the video. For playback, the number one means it was possible to
+        to play or not the video. For __playback, the number one means it was possible to
         play and zero is otherwise.
         """
-        return tuple(self.playback)
+        return tuple(self.__playback)
