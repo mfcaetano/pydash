5e5d4c384b1fbd948755f238875f39ea66a09631
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2021-04-22 12:23:17 -0300

    Changes package to root
    Co-authored-by: Waliff Cordeiro <waliffcordeiro@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydash',
-    packages=find_packages(),
+    packages=[''],
     url='https://github.com/mfcaetano/pydash',
     description='A Framework Based Educational Tool for Adaptive Streaming Video Algorithms Study',
     long_description=open('README.md').read(),
