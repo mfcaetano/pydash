fac78a45fe84b8414fa9c8ae407c238475c70c40
Pedro Augusto Ramalho Duarte <43936240+PedroAugustoRamalhoDuarte@users.noreply.github.com>
2021-04-21 23:40:43 -0300

    Removes dependencies pinned versions

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='pydash',
     packages=find_packages(),
-    url='https://github.abc.com/abc/myabc',
+    url='https://github.com/mfcaetano/pydash',
     description='A Framework Based Educational Tool for Adaptive Streaming Video Algorithms Study',
     long_description=open('README.md').read(),
     install_requires=[
-        "numpy==1.20.2",
-        "matplotlib==3.4.1",
-        "scipy==1.6.2",
-        "seaborn==0.11.1"
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "seaborn"
         ],
 )
